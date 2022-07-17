86593301e6f3e44db90eed19b0066d61d0ba8ec5
ljcucc <ljcucc@gmail.com>
2022-07-17 12:34:27 +0800

    seperate main to client and server side script

---
+++ client.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("client is running...")
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+
 def main():
-    print("program is running...")
+    print("servere is running...")
 
 if __name__ == "__main__":
     main()
