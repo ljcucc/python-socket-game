ae82846cf1c8c1b42885fca7c38cbc6c4015c7a8
ljcucc <ljcucc@gmail.com>
2022-07-19 04:17:51 +0800

    copy-&-paste echo server example from web

--- client.py
+++ client.py
@@ -1,6 +1,23 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+# Copied from https://realpython.com/python-sockets/
+
+def start_client():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b"HELLO WORLD")
+        data = s.recv(1024)
+
+    print(f"Received {data!r}")
+
 
 def main():
     print("client is running...")
+
+    start_client()
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,6 +1,30 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+# Copied from https://realpython.com/python-sockets/
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST,PORT))
+        s.listen()
+        conn, addr = s.accept()
+
+        with conn:
+            print(f"Connected by {addr}")
+
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
 
 def main():
     print("servere is running...")
+    
+    start_server()
 
 if __name__ == "__main__":
     main()
